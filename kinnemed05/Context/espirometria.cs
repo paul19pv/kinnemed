a35e4f2486e6d760b3b5cd1f31b657e9172bca7d
paul_19pv <paul_19pv@hotmail.com>
2016-04-30 13:21:07 -0400

    algo 2

--- kinnemed05/Context/espirometria.cs
+++ kinnemed05/Context/espirometria.cs
@@ -12,7 +12,7 @@ namespace kinnemed05.Models
         [Required(ErrorMessage="El campo Paciente es requerido")]
         public int esp_paciente { get; set; }
         [Display(Name = "Archivo")]
-        public string esp_archivo { get; set; }
+        public string esp_archivo { get; set; } 
         [Display(Name = "Observación")]
         public string esp_observacion { get; set; }
     }
